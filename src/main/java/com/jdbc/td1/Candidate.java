f5b90fa90b6c5252d0765db0d54724b9e744ea03
Zinedis <nantenainabakari@gmail.com>
2026-02-24 20:45:56 +0300

    add class Candidate

---
+++ src/main/java/com/jdbc/td1/Candidate.java
@@ -0,0 +1,17 @@
+package com.jdbc.td1;
+
+public class Candidate {
+    private int id;
+    private String name;
+
+    public Candidate() {}
+    public Candidate(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
